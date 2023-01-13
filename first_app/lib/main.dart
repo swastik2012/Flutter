8271e494909fc2e9ecf2bad738a7c75a7ae0da64
Swastik Juneja <swastikjuneja2012@gmail.com>
2023-01-13 11:19:19 +0530

    second commit

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  
+  runApp(MaterialApp(
+      home: Scaffold(
+    body: Center(
+        child: ElevatedButton(
+      style:
+          ButtonStyle(backgroundColor: MaterialStateColor.resolveWith((states) {
+        return Colors.black;
+      })),
+      onPressed: () => {print("I am pressed")},
+      child: Text(
+        "Press Me",
+        selectionColor: Colors.red,
+      ),
+    )),
+    appBar: AppBar(
+      title: Center(
+        child: Text("My App Bar"),
+      ),
+    ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () => {print("I pressed my floating button")},
+      child: Text("+"),
+    ),
+  )));
+
+  //Material App // Android
+  // Cupurtino   // IOS
 }
