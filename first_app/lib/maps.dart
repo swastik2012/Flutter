5129ce2b546eb260a8f38b205d94a486608a9ce9
Swastik Juneja <swastikjuneja2012@gmail.com>
2023-01-19 04:45:09 +0530

    ...

--- first_app/lib/maps.dart
+++ first_app/lib/maps.dart
@@ -1,9 +1,9 @@
 void main(List<String> args) {
   // Map -> key and value pair
 
-  Map<String, String> myStringMap = 
-  {"1": "Shreyash", "2"};
-  
+  Map<String, String> myStringMap = {"1": "Shreyash", "2": "Hari", "3": "Ravi"};
 
-  print(myStringMap["1"]);
+  myStringMap.forEach((key, value) => print(value));
+
+  // print(myStringMap["1"]);
 }
