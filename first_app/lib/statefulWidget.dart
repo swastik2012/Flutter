6e33b34e1816d3922b8e4d9af26fe07c63657b80
Swastik Juneja <swastikjuneja2012@gmail.com>
2023-01-20 10:44:22 +0530

    statefulWidget.dart added

---
+++ first_app/lib/statefulWidget.dart
@@ -0,0 +1,156 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+// import 'dart:html';
+// import 'dart:js';
+
+// import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+List<Color> myContainerColors = [
+  Color.fromARGB(255, 197, 160, 48),
+  Colors.black,
+  Colors.blue,
+  Colors.green,
+  Colors.pink
+];
+int myNumber = 0;
+
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(body: SafeArea(child: MyCounterStatefulWidget()))));
+
+  //Material App // Android
+  // Cupurtino   // IOS
+}
+
+class MyCounterWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text("Number"),
+          ),
+          Padding(
+            padding: EdgeInsets.all(20),
+            child: Text(myNumber.toString()),
+          ),
+          Padding(
+            padding: EdgeInsets.all(30),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    onPressed: OnPressedMinus,
+                    child: Text("-"),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black45))),
+                SizedBox(
+                  height: 10,
+                  width: 10,
+                ),
+                ElevatedButton(
+                    onPressed: OnPressedPlus,
+                    child: Text("+"),
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Colors.black45)))
+              ],
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+
+
+class MyCounterStatefulWidget extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyCounterWidgetState();
+  }
+}
+
+class MyCounterWidgetState extends State<MyCounterStatefulWidget> {
+  int myNumber = 0;
+  // TODO: implement widget
+  @override
+  Widget build(BuildContext context) {
+    return Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Padding(
+        padding: EdgeInsets.all(20),
+        child: Text("Number"),
+      ),
+      Padding(
+        padding: EdgeInsets.all(20),
+        child: Text(myNumber.toString()),
+      ),
+      Padding(
+        padding: EdgeInsets.all(30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                onPressed: OnPressedMinus,
+                child: Text("-"),
+                style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Colors.black45))),
+            SizedBox(
+              height: 10,
+              width: 10,
+            ),
+            ElevatedButton(
+                onPressed: OnPressedPlus,
+                child: Text("+"),
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(Colors.black45)))
+          ],
+        ),
+      )
+    ]);
+  }
+
+
+  void OnPressedMinus() {
+
+  setState(() {
+    myNumber = myNumber - 1;
+  });
+
+  print("pressed - sign : " + myNumber.toString());
+}
+
+void OnPressedPlus() {
+
+  setState(() {
+    myNumber = myNumber + 1;
+  });
+  print("pressed + sign : " + myNumber.toString());
+}
+
+}
+
+
+void OnPressedMinus() {
+
+  
+
+  
+    myNumber = myNumber - 1;
+ 
+
+  print("pressed - sign : " + myNumber.toString());
+}
+
+void OnPressedPlus() {
+  myNumber = myNumber + 1;
+  print("pressed + sign : " + myNumber.toString());
+}
